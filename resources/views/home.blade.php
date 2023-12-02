52e453e896e7671d875d17d2eda4c56d20a4fa7b
idanpostman <idanpostman@gmail.com>
2023-12-02 13:13:49 +0700

    Menambahkan Responsive Navbar(Logo belum)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2>halaman home</h2>
+
 @endsection
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,25 +1,38 @@
-<nav class="uk-navbar-container">
-    <div class="uk-container">
-        <div uk-navbar>
-            <div class="uk-navbar-left">
-                <a class="uk-navbar-item uk-logo" href="#">
-                    <img src="/img/aurelia.jpg" alt="aurelia-medika" width="200" height="200">
-                </a>
+<header class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+    <div class="container">
+        <div class="flex items-center justify-between relative">
+            <div class="px-4">
+                <a href="#home" class="font-bold text-lg text-primary block py-6">Aurelia Medika</a>
             </div>
-            <div class="uk-navbar-right">
-                <ul class="uk-navbar-nav">
-                    <li class="uk-active"><a href="#">Home</a></li>
-                    <li class="uk-active"><a href="#">About Us</a></li>
-                    <li class="uk-active"><a href="#">Our Doctor</a></li>
-                    <li class="uk-active"><a href="#">Contact</a></li>
-                    <li class="uk-navbar-item">
-                        <button class="uk-button uk-button-default uk-border-pill">Login</button>
-                    </li>
-                    <li class="uk-navbar-item">
-                        <button class="uk-button uk-button-primary uk-border-pill">Register</button>
-                    </li>
-                </ul>
+            <div class="flex items-center px-4">
+                <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden">
+                    <span class="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                    <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                    <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+                </button>
+                <nav id="nav-menu" class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                    <ul class="block lg:flex">
+                        <li class="group">
+                            <a href="#home" class="text-base text-dark py-2 mx-5 flex group-hover:text-primary">Home</a>
+                        </li>
+                        <li class="group">
+                            <a href="#aboutus" class="text-base text-dark py-2 mx-5 flex group-hover:text-primary">About us</a>
+                        </li>
+                        <li class="group">
+                            <a href="#ourdoctor" class="text-base text-dark py-2 mx-5 flex group-hover:text-primary">Our Doctor</a>
+                        </li>
+                        <li class="group">
+                            <a href="#contact" class="text-base text-dark py-2 mx-5 flex group-hover:text-primary">Contact</a>
+                        </li>
+                        <li class="group my-4 mx-10 lg:my-0 lg:mx-4">
+                            <button class="text-base font-semibold text-white bg-primary py-3 px-5 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500 ">Register</button>
+                        </li>
+                        <li class="group mx-10 lg:mx-0">
+                            <button class="text-base font-semibold text-white bg-slate-500 py-3 px-7 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500 ">Login</button>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
-</nav>
+</header>
