4849c3333dd13bab8a07cf30b35383066ecb480f
idanpostman <idanpostman@gmail.com>
2023-12-25 13:14:23 +0700

    benerin sidebar

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -11,7 +11,7 @@
              </a>
           </li>
           <li>
-               <button type="button" class="flex items-center w-full p-2 text-white opacity-75 hover:opacity-100 pt-3 transition rounded-lg group hover:bg-[#003049] active:bg-[#003049]" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+               <button type="button" class="flex items-center w-full p-2 text-white opacity-75 hover:opacity-100 pt-3 transition rounded-lg group hover:bg-[#003049] active:bg-[#003049] " aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                   <svg class="w-5 h-5 pacity-75 hover:opacity-100 transition duration-75 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 19">
                      <path fill="#ffffff" d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
                      <path fill="#ffffff" d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
@@ -21,7 +21,7 @@
                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
                </button>
-               <ul id="dropdown-example" class="hidden py-2 space-y-2">
+               <ul id="dropdown-example" class="hidden py-2 space-y-2 overflow-hidden">
                   <li>
                      <a href="/dashboard/antrian-poli-umum" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group dark:text-white hover:bg-[#003049] active:bg-[#003049]"">Poli Umum</a>
                   </li>
@@ -47,49 +47,9 @@
                <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
                   <path fill="#ffffff" d="M142.4 21.9c5.6 16.8-3.5 34.9-20.2 40.5L96 71.1V192c0 53 43 96 96 96s96-43 96-96V71.1l-26.1-8.7c-16.8-5.6-25.8-23.7-20.2-40.5s23.7-25.8 40.5-20.2l26.1 8.7C334.4 19.1 352 43.5 352 71.1V192c0 77.2-54.6 141.6-127.3 156.7C231 404.6 278.4 448 336 448c61.9 0 112-50.1 112-112V265.3c-28.3-12.3-48-40.5-48-73.3c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V336c0 97.2-78.8 176-176 176c-92.9 0-168.9-71.9-175.5-163.1C87.2 334.2 32 269.6 32 192V71.1c0-27.5 17.6-52 43.8-60.7l26.1-8.7c16.8-5.6 34.9 3.5 40.5 20.2zM480 224a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/>
                </svg>
-               <span class="ms-3">Dashboard</span>
+               <span class="ms-3">Dokter</span>
             </a>
          </li>
-         <li>
-              <button type="button" class="flex items-center w-full p-2 text-white opacity-75 hover:opacity-100 pt-3 transition rounded-lg group hover:bg-[#003049] active:bg-[#003049]" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                 <svg class="w-5 h-5 pacity-75 hover:opacity-100 transition duration-75 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 19">
-                    <path fill="#ffffff" d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
-                    <path fill="#ffffff" d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
-                 </svg>
-                 <span class="ms-3">Daftar Antrian</span>
-                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                 </svg>
-              </button>
-              <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                 <li>
-                    <a href="/dashboard/antrian-poli-umum" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group dark:text-white hover:bg-[#003049] active:bg-[#003049]"">Poli Umum</a>
-                 </li>
-                 <li>
-                    <a href="/dashboard/antrian-poli-gigi" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group dark:text-white hover:bg-[#003049] active:bg-[#003049]">Poli Gigi</a>
-                 </li>
-                 <li>
-                    <a href="/dashboard/antrian-poli-tht" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group  dark:text-white hover:bg-[#003049] active:bg-[#003049]">Poli THT</a>
-                 </li>
-              </ul>
-           </a>
-        </li>
-         <li>
-           <a href="/dashboard/pasien" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
-              <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
-                 <path fill="#ffffff" d="M48 0C21.5 0 0 21.5 0 48V256H144c8.8 0 16 7.2 16 16s-7.2 16-16 16H0v64H144c8.8 0 16 7.2 16 16s-7.2 16-16 16H0v80c0 26.5 21.5 48 48 48H265.9c-6.3-10.2-9.9-22.2-9.9-35.1c0-46.9 25.8-87.8 64-109.2V271.8 48c0-26.5-21.5-48-48-48H48zM152 64h16c8.8 0 16 7.2 16 16v24h24c8.8 0 16 7.2 16 16v16c0 8.8-7.2 16-16 16H184v24c0 8.8-7.2 16-16 16H152c-8.8 0-16-7.2-16-16V152H112c-8.8 0-16-7.2-16-16V120c0-8.8 7.2-16 16-16h24V80c0-8.8 7.2-16 16-16zM512 272a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM288 477.1c0 19.3 15.6 34.9 34.9 34.9H541.1c19.3 0 34.9-15.6 34.9-34.9c0-51.4-41.7-93.1-93.1-93.1H381.1c-51.4 0-93.1 41.7-93.1 93.1z"/>
-             </svg>
-              <span class="ms-3">Pasien</span>
-           </a>
-        </li>
-        <li>
-           <a href="#" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
-              <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
-                 <path fill="#ffffff" d="M142.4 21.9c5.6 16.8-3.5 34.9-20.2 40.5L96 71.1V192c0 53 43 96 96 96s96-43 96-96V71.1l-26.1-8.7c-16.8-5.6-25.8-23.7-20.2-40.5s23.7-25.8 40.5-20.2l26.1 8.7C334.4 19.1 352 43.5 352 71.1V192c0 77.2-54.6 141.6-127.3 156.7C231 404.6 278.4 448 336 448c61.9 0 112-50.1 112-112V265.3c-28.3-12.3-48-40.5-48-73.3c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V336c0 97.2-78.8 176-176 176c-92.9 0-168.9-71.9-175.5-163.1C87.2 334.2 32 269.6 32 192V71.1c0-27.5 17.6-52 43.8-60.7l26.1-8.7c16.8-5.6 34.9 3.5 40.5 20.2zM480 224a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/>
-              </svg>
-              <span class="ms-3">Dokter</span>
-           </a>
-        </li>
         <li>
            <a href="#" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
               <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true"  xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
@@ -106,7 +66,7 @@
               <span class="ms-3">Obat</span>
            </a>
         </li>
-         
+
       </ul>
    </div>
 </aside>
