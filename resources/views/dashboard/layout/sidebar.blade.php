4e59b8718de65c1e79e7130d974e666861e2a6da
Priamitra12 <wsalif48@gmail.com>
2023-12-30 03:52:08 +0700

    Menambahkan Tampilan Halaman Obat

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -51,7 +51,7 @@
             </a>
          </li>
         <li>
-           <a href="#" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
+           <a href="/dashboard/rekam-medis" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
               <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true"  xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
                  <path fill="#ffffff" d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/>
               </svg>
@@ -59,7 +59,7 @@
            </a>
         </li>
         <li>
-           <a href="#" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
+           <a href="/dashboard/obat" class="flex items-center p-2 text-white opacity-75 hover:opacity-100 pt-3 rounded-lg  hover:bg-[#003049] active:bg-[#003049]  group">
               <svg class="w-5 h-5 text-white opacity-75 hover:opacity-100 transition duration-75  " aria-hidden="true"  xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
                  <path fill="#ffffff" d="M112 96c-26.5 0-48 21.5-48 48V256h96V144c0-26.5-21.5-48-48-48zM0 144C0 82.1 50.1 32 112 32s112 50.1 112 112V368c0 61.9-50.1 112-112 112S0 429.9 0 368V144zM554.9 399.4c-7.1 12.3-23.7 13.1-33.8 3.1L333.5 214.9c-10-10-9.3-26.7 3.1-33.8C360 167.7 387.1 160 416 160c88.4 0 160 71.6 160 160c0 28.9-7.7 56-21.1 79.4zm-59.5 59.5C472 472.3 444.9 480 416 480c-88.4 0-160-71.6-160-160c0-28.9 7.7-56 21.1-79.4c7.1-12.3 23.7-13.1 33.8-3.1L498.5 425.1c10 10 9.3 26.7-3.1 33.8z"/>
               </svg>
