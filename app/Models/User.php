5a1fc4e16d6853899c0630a18d8682ad02f81b5d
Priamitra12 <wsalif48@gmail.com>
2023-12-30 04:12:13 +0700

    Memperbaiki No Live Search dan Akses Dropdown Dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function Admin()
+{
+    return $this->role === 'admin'; // Sesuaikan ini dengan struktur peran pengguna Anda
+}
 }
