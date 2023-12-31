74608017c72bb50e636224d1455094273503a575
Priamitra12 <wsalif48@gmail.com>
2023-12-31 23:38:19 +0700

    antrian umum update

--- app/Http/Controllers/DashboardAntrianUmumController.php
+++ app/Http/Controllers/DashboardAntrianUmumController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Antrian;
 
 class DashboardAntrianUmumController extends Controller
 {
     public function index()
-   {
-    return view('dashboard.antrianumum');
-   }
+{
+    $antrians = Antrian::where('poli', 'Poli Umum')->get();
+
+    return view('dashboard.antrianumum', ['antrians' => $antrians]);
+}
+public function destroy($antrianId)
+{
+    try {
+        // Find the Antrian based on ID
+        $antrian = Antrian::findOrFail($antrianId);
+
+        // Perform the deletion logic, for example:
+        $antrian->delete();
+
+        // Redirect with success message
+        return redirect('/dashboard/antrian-poli-umum')->with('success', 'Antrian berhasil dihapus');
+    } catch (\Exception $e) {
+        // Handle the error if encountered, for example:
+        return redirect('/dashboard/antrian-poli-umum')->with('error', 'Gagal menghapus antrian');
+    }
+}
+
 }
--- resources/views/dashboard/antrianumum.blade.php
+++ resources/views/dashboard/antrianumum.blade.php
@@ -11,34 +11,29 @@
         <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white ">
             Antrian Poli Umum Hari Ini
             <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-5 items-center justify-between pb-4">
-
-                 <!-- pencarian -->
-                <label for="table-search" class="sr-only">Cari</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Cari Antrian">
-                </div>
-                
                 <!-- Button Tambah Antrian -->
-                <button class="absolute top-0 right-8 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"> + Tambah Antrian</button>
+                <a href="/dashboard/antrian/create">
+                    <button class="absolute top-8 right-8 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"> + Tambah Antrian</button>
+                </a>
             </div>
         </caption>
 
         <table class="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-gray-100 "><tr>
                 <th scope="col" class="px-6 py-3">
-                    No Antrian
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    No Registrasi
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    No Telephone
+                    No
                 </th>
                 <th scope="col" class="px-6 py-3">
                     Nama Pasien
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    No Telpon
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Poli
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    No Antrian
                 </th>
                 <th scope="col" class="px-6 py-3 text-center">
                     Aksi
@@ -47,37 +42,125 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b ">
-                <th scope="row" class="px-6 py-4 font-medium text-primary whitespace-nowrap">
-                    01
-                </th>
-                <td class="px-6 py-4">
-                    2156738
-                </td>
-                <td class="px-6 py-4">
-                    0812345678
-                </td>
-                <td class="px-6 py-4">
-                    Moch Priamitra Nur Alif
-                </td>
+            @foreach($antrians as $antrian)
+                <tr class="bg-white border-b">
+                    <th scope="row" class="px-6 py-4 font-medium text-primary whitespace-nowrap">
+                        {{ $loop->iteration }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $antrian->nama }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $antrian->no_telp }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $antrian->poli }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ "{$antrian->kode_poli} - {$antrian->nomor_antrian}" }}
+                    </td>
                 <td class="px-3 py-4 text-center">
-                    <button type="button" class="text-white bg-green-400 hover:bg-green-500/90 focus:ring-4 focus:outline-none focus:ring-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-500/55 me-2 mb-2">
-                        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-500/90 focus:ring-4 focus:outline-none focus:ring-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-500/55 me-2 mb-2"
+                    onclick="playNotificationSound('{{ $antrian->kode_poli }}', '{{ $antrian->nomor_antrian }}')">
+                        <svg class="w-4 h-4 mb-1 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path fill="#ffffff" d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75H192 160 64c-35.3 0-64 28.7-64 64v96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V352l8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V300.4c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4V32zm-64 76.7V240 371.3C357.2 317.8 280.5 288 200.7 288H192V192h8.7c79.8 0 156.5-29.8 215.3-83.3z"/>
                         </svg>
-                        Panggil
                     </button>
-                    <button type="button" class="text-white border-red-700 bg-red-600 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center  dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->\
-                            <path fill="#ffffff" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                        </svg>
-                        <span class="sr-only"></span>
+                    <button type="button" class="text-white bg-green-400 hover:bg-green-600/90 focus:ring-4 focus:outline-none focus:ring-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-500/55 me-2 mb-2"
+                            onclick="moveToRekamMedis('{{ $antrian->id }}')">
+                        Ada
                     </button>
+                    <form action="{{ url('/dashboard/antrian-poli-umum/' . $antrian->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-white bg-red-700 hover:bg-red-600/90 focus:ring-4 focus:outline-none focus:ring-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-500/55 me-2 mb-2">
+                            Tidak Ada
+                        </button>
+                    </form>
                 </td>
             </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
+<audio id="notificationSound">
+    <source src="/path-to-your-sound-file.mp3" type="audio/mp3">
+    Your browser does not support the audio element.
+</audio>
+<script>
+    function moveToRekamMedis(antrianId) {
+        if (confirm('Apakah Anda yakin ingin memindahkan data ke rekam medis?')) {
+            window.location.href = '/dashboard/antrian/move-to-rekam-medis/' + antrianId;
+        }
+    }
 
+    function playNotificationSound(kode_poli, nomor_antrian) {
+    var audioPath = "/sound/";
+
+    // Mapping for the sounds based on digits
+    var digitSounds = {
+        '0': 'nol',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9'
+    };
+
+    // Mapping for special cases
+    var specialCases = {
+        '10': 'sepuluh',
+        '11': 'sebelas'
+    };
+
+    function playNext(index) {
+        if (index < audioFiles.length) {
+            var audio = new Audio(audioFiles[index]);
+            audio.addEventListener('ended', function () {
+                playNext(index + 1);
+            });
+            audio.play();
+        }
+    }
+
+    // Define the sequence of audio files
+    var audioFiles = [
+        audioPath + "Airport_Bell.mp3",
+        audioPath + "Nomor_antrian.mp3",
+        audioPath + kode_poli.toLowerCase() + ".mp3",
+    ];
+
+    // Add audio files for special cases
+    if (specialCases.hasOwnProperty(nomor_antrian)) {
+        audioFiles.push(audioPath + specialCases[nomor_antrian] + ".mp3");
+    } else {
+        // Handle tens and units separately
+        var tens = Math.floor(nomor_antrian / 10);
+        var units = nomor_antrian % 10;
+
+        // Add audio files for tens
+        if (tens > 1) {
+            audioFiles.push(audioPath + digitSounds[tens] + ".mp3");
+            audioFiles.push(audioPath + "puluh.mp3");
+        }
+
+        // Add audio files for units
+        if (units > 0) {
+            audioFiles.push(audioPath + digitSounds[units] + ".mp3");
+        }
+    }
+
+    // Add the "Masuk ke Ruangan" sound
+    audioFiles.push(audioPath + "masuk_ke_ruangan.mp3");
+
+    // Start playing the first audio file
+    playNext(0);
+
+}
+</script>
 
 @endsection
